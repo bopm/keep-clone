ee3552a3d09d5a7d192849856cdcf15a3d462c99
Sergey Moiseev <moiseev.sergey@gmail.com>
2017-08-15 02:32:50 +0300

    Day 2. Template driven form.

--- src/app/new-card-input/new-card-input.component.ts
+++ src/app/new-card-input/new-card-input.component.ts
@@ -1,12 +1,16 @@
-import {Component, EventEmitter, HostBinding, HostListener, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, HostBinding, HostListener, OnInit, Output, ViewChild} from '@angular/core';
 import {card} from '../models/card';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-new-card-input',
   template: `
     <div class="card">
       <div class="card-block">
-        <input placeholder="Take a note..." class="form-control" [(ngModel)]="newCard.text">
+        <form #form="ngForm">
+          <input placeholder="Take a note..." class="form-control" name="text" [(ngModel)]="newCard.text" required>
+        </form>
+        {{ form.value | json }}
       </div>
     </div>
   `,
@@ -18,12 +22,14 @@ export class NewCardInputComponent implements OnInit {
   @HostBinding('class') class = 'col-8';
   @Output() onCardAdd = new EventEmitter<string>();
 
+  @ViewChild('form') public form: NgForm;
+
   public newCard: card = {text: ''};
 
   @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.charCode === 13 && this.newCard.text.length > 0) {
-      this.addCard(this.newCard.text);
+    if (event.charCode === 13 && this.form.valid) {
+      this.addCard(this.form.value.text);
     }
   }
 
